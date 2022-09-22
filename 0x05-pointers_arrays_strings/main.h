4582ce4c158aee4cd368326b48610676cbb6fc07
Rayobino8141 <greatrayobino@gmail.com>
2022-09-22 07:49:50 -0700

    lord of all

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,4 @@
-#ifndef MAIN_H 
- #define MAIN_H 
- #include <stdio.h> 
-  
- /** 
-  * void prototypes() 
-  *char prototypes() 
-  * int prototypes() 
-  */ 
-  
+int _putchar(); 
  void reset_to_98(int *n); 
  void swap_int(int *a, int *b); 
  int _strlen(char *s); 
@@ -17,7 +8,4 @@
  void puts2(char *str); 
  void puts_half(char *str); 
  void print_array(int *a, int n); 
- char *_strcpy(char *dest, char *src); 
- int _atoi(char *s); 
-  
- #endif /*MAIN_H*/
+ char *_strcpy(char *dest, char *src);
